35f6b6918e0897cf1a91eb822cf952904f516c0a
ZefanyaPrasetiyo <zefanyaprasetiyo29>
2025-10-05 09:31:16 +0700

    ubah navbar

--- src/app/componen/landingpage/Navbar.jsx
+++ src/app/componen/landingpage/Navbar.jsx
@@ -13,7 +13,7 @@ export default function Navbar() {
         
         {/* Logo */}
         <div className="flex items-center gap-2">
-          <span className="text-green-600 text-2xl">🌱</span>
+          <svg xmlns="http://www.w3.org/2000/svg" className="w-12 h-12" viewBox="0 0 24 24"><path fill="currentColor" d="M5.998 3a7 7 0 0 1 6.913 5.895A6.48 6.48 0 0 1 17.498 7h4.5v2.5a6.5 6.5 0 0 1-6.5 6.5h-2.5v5h-2v-8h-2a7 7 0 0 1-7-7V3zm14 6h-2.5a4.5 4.5 0 0 0-4.5 4.5v.5h2.5a4.5 4.5 0 0 0 4.5-4.5zm-14-4h-2v1a5 5 0 0 0 5 5h2v-1a5 5 0 0 0-5-5"/></svg>
           <span className="font-semibold text-green-600 text-2xl">reforesta</span>
         </div>
 
@@ -22,7 +22,7 @@ export default function Navbar() {
           <li>
             <Link
               href="/"
-              className={`px-4 py-1 rounded-full transition font-semibold ${
+              className={`px-6 py-2 rounded-full transition font-semibold ${
                 pathname === "/"
                   ? "bg-green-600 text-white"
                   : "text-green-700 hover:text-green-800"
