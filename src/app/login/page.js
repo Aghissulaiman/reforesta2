abd9e806f23bfcf5b85a5ff11863900a29eef9f3
Alfianpiyan <alfianalfatizi@gmail.com>
2025-10-05 09:16:22 +0700

    perubahan kedua login regis

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -104,7 +104,7 @@ export default function Login() {
 
           <div className="mt-6">
             <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-[#4CAF50] rounded-lg hover:bg-[#69cd6d] focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
-              Sign In
+              Masuk
             </button>
           </div>
 
